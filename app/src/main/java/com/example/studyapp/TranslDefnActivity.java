c2e7c5ba7213741b38e9c46f79b7754b30ae508c
Shawn-Hanclock <nwahs007@gmail.com>
2022-06-03 08:28:54 -0700

    feat: created translate or define activity

---
+++ app/src/main/java/com/example/studyapp/TranslDefnActivity.java
@@ -0,0 +1,13 @@
+package com.example.studyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranslDefnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transl_defn);
+    }
+}
